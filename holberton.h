83f4c2b307731f4a728a32d3bd249b2bf1d8a4c5
nickarshadi <1477@holbertonschool.com>
2020-03-16 10:15:19 -0500

    put

--- holberton.h
+++ holberton.h
@@ -1,6 +1,21 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+/**
+ * strukt Struktur - Struktur fuer special characters
+ * @sp: special characters
+ * @f: function make action for special characters
+ */
+typedef struct Struktur1   /*struktur special characters*/
+{
+	char *sp;
+	void (*f)(char *string);
+} struktur1;
+typedef struct Struktur2
+{
+	char *fo;
+	void (*f)(char *string);
+} struktur2;
 
-int putchar(char c);
+int _putchar(char c);
 
 #endif
--- printf.c
+++ printf.c
@@ -1,2 +1,57 @@
-#include <stdargs.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <holberton.h>
+/**
+ * _printf - printff function
+ * @format: format string first printf string
+ * Prototype: int _printf(const char *format, ...);
+ * Returns: the number of characters printed 
+ *(excluding the null byte used to end output to strings)
+ * write output to stdout, the standard output stream
+ * format is a character string. The format string is
+ * composed of zero or more directives.
+ * See man 3 printf for more detail. You need to handle
+ * the following conversion specifiers:
+ * c s %
+ **/
+int _printf(const char *format, ...);
+{
+	struktur1 special[] = {
+	{"a", _putchar},
+	{"b", _putchar},
+	{"f", _putchar},
+	{"n", _putchar},
+	{"r", _putchar},
+	{"t", _putchar},
+	{"v", _putchar}
+	{NULL, NULL}
+	};
+	struktur2 form[] = {
+	{"c", pchar},
+	{"s", pstring},
+	{"%", pmod},
+	{NULL, NULL}
+	}
+	char *string = format;
+	int a, b, c, d, e;
+	va_list valist;
+
+	va_start(valist, format);
+	for(a = 1; string[a + 1] != '0'; a++)  /*run through whole strukt*/
+	{
+		if (string[a] == "\\")
+		{
+			for (b = 0; b < 3; b++)
+			{
+				if (string[a + 1] == *struktur1[b].sp)
+				{
+					struktur1[b].f(*struktur1[b].sp)
+					a++;
+				}
+			}
+		}	
+	}
+	va_end(valist);
+	return (0);
+}

d3859203af8b707381942c94550e38ec6b75ff54
nickarshadi <1477@holbertonschool.com>
2020-03-17 10:43:57 -0500

    printf

--- printf.c
+++ printf.c
@@ -17,41 +17,41 @@
  **/
 int _printf(const char *format, ...);
 {
-	struktur1 special[] = {
-	{"a", _putchar},
-	{"b", _putchar},
-	{"f", _putchar},
-	{"n", _putchar},
-	{"r", _putchar},
-	{"t", _putchar},
-	{"v", _putchar}
-	{NULL, NULL}
-	};
-	struktur2 form[] = {
-	{"c", pchar},
-	{"s", pstring},
-	{"%", pmod},
-	{NULL, NULL}
-	}
-	char *string = format;
-	int a, b, c, d, e;
+	int i = 0;
+	char *buffer = format;
 	va_list valist;
-
+	
 	va_start(valist, format);
-	for(a = 1; string[a + 1] != '0'; a++)  /*run through whole strukt*/
+	if (buffer == NULL)
 	{
-		if (string[a] == "\\")
-		{
-			for (b = 0; b < 3; b++)
-			{
-				if (string[a + 1] == *struktur1[b].sp)
-				{
-					struktur1[b].f(*struktur1[b].sp)
-					a++;
-				}
-			}
-		}	
+		return (-1);
 	}
+	if (buffer == "%")
+	{
+		return (-1);
+	}
+	for (i = 0; buffer[i] != '0'; i++)
+	{
+		if (buffer[i] == '%')
+		{
+			switch(buffer[i + 1])
+			{
+				case 'c' :
+				buffer = save_c(buffer, i, valist);
+				break;
+				case 's' :
+				buffer = save_s(buffer, i, va_list);
+				break;
+				case 'd' :
+				buffer = save_d(buffer, i, va_list);
+				break;
+				case 'i' :
+				buffer = save_i(buffer, i, va_list);
+				break;
+			}
+		}
+	}
+	puts(buffer);
 	va_end(valist);
-	return (0);
+	return (buffer);
 }

552a9553ad87c40e8ca9f6f769ce9470fe9a874b
nickarshadi <1477@holbertonschool.com>
2020-03-17 12:38:03 -0500

    betty

--- printf.c
+++ printf.c
@@ -7,51 +7,48 @@
  * _printf - printff function
  * @format: format string first printf string
  * Prototype: int _printf(const char *format, ...);
- * Returns: the number of characters printed 
+ * Returns: the number of characters printed
  *(excluding the null byte used to end output to strings)
  * write output to stdout, the standard output stream
  * format is a character string. The format string is
  * composed of zero or more directives.
  * See man 3 printf for more detail. You need to handle
  * the following conversion specifiers:
+ * Return: return the string
  * c s %
  **/
 int _printf(const char *format, ...)
 {
 	int i = 0;
-	char *buffer = malloc(_strlen((char *)format) * sizeof (char) + 1);
+	char *buffer = malloc(_strlen((char *)format) * sizeof(char) + 1);
 	va_list valist;
 
 	while (format[i])
-    {
-        buffer[i] = format[i];
-        i++;
-    }
+	{
+		buffer[i] = format[i];
+		i++;
+	}
 	va_start(valist, format);
 	if (buffer == NULL)
-	{
 		return (-1);
-	}
 	if (*buffer == '%' && *(buffer + 1) == 0)
-	{
 		return (-1);
-	}
 	for (i = 0; format[i] != '0'; i++)
 	{
 		if (buffer[i] == '%')
 		{
-			switch(buffer[i + 1])
+			switch (buffer[i + 1])
 			{
-				case 'c' :
+				case 'c':
 				buffer = save_c(buffer, i - 1, va_arg(valist, int));
 				break;
-				case 's' :
+				case 's':
 				buffer = save_s(buffer, i - 1, va_arg(valist, char *));
 				break;
-				case 'd' :
+				case 'd':
 				buffer = save_d(buffer, i - 1, va_arg(valist, int));
 				break;
-				case 'i' :
+				case 'i':
 				buffer = save_d(buffer, i - 1, va_arg(valist, int));
 				break;
 				default:

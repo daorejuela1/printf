a137f00b815182db89d44462df1b94799376cf2b
daorejuela1 <vids@hotmail.es>
2020-03-17 12:12:40 -0500

    Eliminated frees inside functions

---
+++ holberton2.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+char * save_d (char * buffer, int position, int data);
+char * save_c (char * buffer, int position, char data);
+char * save_s (char * buffer, int position, char * data);
+void put_in_place(char * new_buffer, char * buffer, int position, int data_len);
+void _strncpy(char *dest, char *src, int init, int end, int data_len);
+int _strlen(char *s);
+char * c_number_to_arr(char * array_data, int n);
+unsigned int cal_digits(unsigned int n);

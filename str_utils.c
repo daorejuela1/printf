eb8fb2e8c7bb32119f52ee549641cf3eb71d5bb5
daorejuela1 <vids@hotmail.es>
2020-03-17 22:40:21 -0500

    Fixed comments for cal digits

--- str_utils.c
+++ str_utils.c
@@ -101,9 +101,9 @@ void _strncpy(char *dest, char *src, int init, int end, int data_len)
  *put_in_place - copies the start and enf of an array that sorrounds a new word
  *@new_buffer: destiny array to copy the string
  *@buffer: source string being copy
- *@position: initial index
+ *@position: initial index.
  *@data_len: lenght of the buffer not counting NULL
- *Return: Nothing
+ *Return: Nothing.
  */
 void put_in_place(char *new_buffer, char *buffer, int position, int data_len)
 {
